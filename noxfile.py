437bc00ffb454a9b1023bd64f4a6fa1c28583663
Gustavo6046 <rehermann6046@gmail.com>
2021-01-14 22:09:20 -0300

    Updated dependencies and noxfile

--- noxfile.py
+++ noxfile.py
@@ -1,16 +1,29 @@
 import nox
+import nox_poetry.patch
 
 
 @nox.session(python=['3.6', '3.7', '3.8', '3.9'])
-def test(session):
+def test(session: nox.sessions.Session):
+    """
+    Execute Pytest unit tests.
+    """
+
+    session.install(".") # dependencies
+
     session.install("pytest")
     session.run("pytest")
 
 @nox.session(python=['3.6', '3.7', '3.8', '3.9'])
-def lint(session):
+def lint(session: nox.sessions.Session):
+    """
+    Execute several linting checks: pylint, mypy, isort, and black.
+    """
+
+    session.install(".") # dependencies
+
     session.install("pylint", "mypy", "isort", "black")
+
     session.run("pylint", "vanquisher")
     session.run("mypy", "vanquisher")
     session.run("isort", "-c", "vanquisher")
     session.run("black", "-c", "vanquisher")
-
