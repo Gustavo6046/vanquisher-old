b8c6aafed22ab5261b3780c9063f2b886fdec1b5
Gustavo6046 <rehermann6046@gmail.com>
2021-01-15 22:41:26 -0300

    Created renderer barebones structure, also network will be its own subpackage

---
+++ vanquisher/network/__init__.py
@@ -0,0 +1,4 @@
+"""
+The Python implementation of the networking protocol that Vanquisher
+uses to communicate the playsim state between server and clients.
+"""
---
+++ vanquisher/renderer/__init__.py
@@ -0,0 +1,5 @@
+"""
+The renderer code.
+
+The terrain is drawn using raymarching.
+"""
