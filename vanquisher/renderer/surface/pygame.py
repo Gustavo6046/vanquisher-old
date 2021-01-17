9b0ff49b86dcd391fa090f2eba8763e90ad2d0cc
Gustavo6046 <rehermann6046@gmail.com>
2021-01-17 04:47:21 -0300

    Fixed a nasty typo and neated docstrings in renderer/surface/pygame.py

--- vanquisher/renderer/surface/pygame.py
+++ vanquisher/renderer/surface/pygame.py
@@ -11,9 +11,7 @@ from . import FramebufferSurface
 
 
 class PygameSurface(FramebufferSurface):
-    """
-    A pygame surface, used when rendering the
-    game to a pygame window.
+    """A pygame surface, used when rendering the game to a pygame window.
 
     __init__ takes the same parameters as
     pygame.display.set_mode; see the pygame
@@ -21,16 +19,12 @@ class PygameSurface(FramebufferSurface):
     """
 
     def __init__(self, *args, **kwargs):
-        """
-        Initializes this surface, including initializing
-        the underlying Pygame surface.
-        """
+        """Initializes this surface, including initializing the underlying Pygame surface."""
 
         self.surf = pygame.display.set_mode(*args, **kwargs)
 
     def get_size(self) -> typing.Tuple[int, int]:
-        """
-        Returns the size of the pygame window.
+        """Returns the size of the pygame window.
 
         For all intents and purposes, this is the size
         that the renderer uses.
@@ -39,7 +33,8 @@ class PygameSurface(FramebufferSurface):
         return pygame.display.get_window_size()
 
     def plot_pixel(self, x: int, y: int, rgb: typing.Tuple[float, float, float]):
-        """
+        """Plots a pixel to the Pygame window.
+
         Plots an RGB pixel at the specified position with the
         specified colour, within the pygame window.
         """
@@ -51,7 +46,8 @@ class PygameSurface(FramebufferSurface):
         xy2: typing.Tuple[int, int],
         rgb: typing.Tuple[float, float, float],
     ):
-        """
+        """Fills a rectangular region with a given colour.
+
         This implementation uses pygame's utilities to fill
         a rectangle of the specified colour between the
         specified corners.
@@ -59,7 +55,5 @@ class PygameSurface(FramebufferSurface):
         self.surf.fill(rgb, (*xy1, *xy2))
 
     def update(self):
-        """
-        Updates the pygame surface.
-        """
-        pygame.display.updat()
+        """Updates the pygame surface."""
+        pygame.display.update()
