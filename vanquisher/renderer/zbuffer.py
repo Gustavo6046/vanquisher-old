400dce26ead75738bf068924e4993caafb46577f
Gustavo6046 <rehermann6046@gmail.com>
2021-01-18 18:49:33 -0300

    SurfaceZBufferWrapper: more typing and more surface interface compliance

--- vanquisher/renderer/zbuffer.py
+++ vanquisher/renderer/zbuffer.py
@@ -12,7 +12,36 @@ class ZBufferSurfaceWrapper:
     def __init__(self, surface: "FramebufferSurface"):
         """Instantiates the Z-buffer wrapper with an underlying backend."""
         self.surface = surface
-        self.z_buffer = {}
+        self.z_buffer: typing.Dict[typing.Tuple[int, int], float] = {}
+
+    def get_size(self) -> typing.Tuple[int, int]:
+        """Regular surface size getter.
+
+        This simply calls the underlying surface's get_size;
+        this exists only for FramebufferSurface compliance.
+        """
+        return self.surface.get_size()
+
+    def plot_pixel(self, x: int, y: int, rgb: typing.Tuple[float, float, float]):
+        """Regular pixel plotting method.
+
+        This simply calls the underlying surface's plot_pixel;
+        this exists only for FramebufferSurface compliance.
+        """
+        self.surface.plot_pixel(x, y, rgb)
+
+    def plot_rect(
+        self,
+        xy1: typing.Tuple[int, int],
+        xy2: typing.Tuple[int, int],
+        rgb: typing.Tuple[float, float, float],
+    ):
+        """Regular rectangle filling method.
+
+        This simply calls the underlying surface's plot_rect;
+        this exists only for FramebufferSurface compliance.
+        """
+        self.surface.plot_rect(xy1, xy2, rgb)
 
     def plot_pixel_z(
         self, x: int, y: int, depth: float, rgb: typing.Tuple[float, float, float]
